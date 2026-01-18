6b01ab0f68615cc6949fa99d7c630d1aaedcab1d
Daniel <91299139+freaxs27@users.noreply.github.com>
2026-01-18 21:52:17 +0700

    Update data source to 'data_wisata_bogor_finalz.csv'

--- coba.py
+++ coba.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_excel('data_wisata_bogor.xlsx')
+df = pd.read_excel('data_wisata_bogor_finalz.csv')
 
 df_clean = df.drop_duplicates(subset=['nama_tempat_wisata'], keep='first')
 
@@ -9,5 +9,6 @@ df_clean["harga_tiket"] = pd.to_numeric(df["harga_tiket"], errors="coerce")
 df = df_clean[df_clean["jumlah_rating"] > 50]
 
 print(df.reset_index)
+
 
 

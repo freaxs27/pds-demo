df3846c9b9ecd7ecb6b1873de8cdf2e6557a09c2
Daniel <91299139+freaxs27@users.noreply.github.com>
2026-01-18 21:50:53 +0700

    Refactor data loading and cleaning for wisata data
    Updated data source to Excel and cleaned the DataFrame by removing duplicates and converting ticket prices to numeric.

--- coba.py
+++ coba.py
@@ -1,27 +1,13 @@
-import streamlit as st 
 import pandas as pd
 
-data = pd.read_csv('data_wisata_bogor.csv')
+df = pd.read_excel('data_wisata_bogor.xlsx')
 
-df = pd.DataFrame(data)
+df_clean = df.drop_duplicates(subset=['nama_tempat_wisata'], keep='first')
 
-df_preferensi = df["preferensi"].value_counts()
-df_kategori = df["kategori"].value_counts()
-df_kecamatan = df["kecamatan"].value_counts()
+df_clean["harga_tiket"] = pd.to_numeric(df["harga_tiket"], errors="coerce")
 
-df_rating4 = df[df["rating"] >= 4.0]
-df_baik = df_rating4[df["jumlah_rating"] > 497].reset_index()
+df = df_clean[df_clean["jumlah_rating"] > 50]
 
-harits = df["jumlah_rating"].mean()
+print(df.reset_index)
 
-st.title('Data Wisata Bogor')
-st.write(df)
-st.title("kategori")
-st.write(df_kategori)
-st.title("kecamatan")
-st.write(df_kecamatan)
-st.title("rating > 4")
-st.write(df_rating4)
-st.title("rating jumlah rating > 1000")
-st.write(df_baik)
-st.write(harits)
+
